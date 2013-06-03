5ff4aee73c2837f022cce57a241a1651adb91d1c
James Michael DuPont <JamesMikeDuPont@gmail.com>
2013-06-03 07:44:56 -0500

    more details

--- treedumpparse.cpp
+++ treedumpparse.cpp
@@ -68,6 +68,10 @@ namespace client
     std::cout << "handle strg:"<< s << std::endl;
   }
 
+  void handle_any(utree s) {
+    std::cout << "handle any:"<< s << std::endl;
+  }
+
   void foo(char c) {
     std::cout << "char 1:"<< c << std::endl;
   }
@@ -86,29 +90,43 @@ namespace client
     {
         tree_dump_file()   : tree_dump_file::base_type(either_lines)
         {  
-
-
+	  
 	  filespec     =  
-	    lit('<') >>
-	    +(char_("-.a-zA-Z0-9"))  >>
-	    lit('>') >> 
-	    lit(':') >> 
-	    +digit ;
+	     (string("<built-in>:0") |
+	      (
+	       +(char_("-.a-zA-Z0-9"))  >>
+	       lit(':') >> 
+	       +digit
+	       )
+	      )
+	    ;
  
+	  identifier_node =(
+			    ( 
+			     strg_field
+			     >>  lngt_field 
+			      )
+			    |    note_field
+			    );
+	  
+
 	  node_type = 
-	    string("namespace_decl")[some_node_type] >> name_field >> scope_field >> source_field
-	    | 
-	    string("identifier_node")[some_node_type] 
-						     >>  strg_field 
-						     >>  lngt_field 
+	     string("namespace_decl")[some_node_type] >> name_field >> scope_field >> source_field
+	     |  string("identifier_node")   >> identifier_node
 	    | string("translation_unit_decl")
 
-	    // generic handler for anything else
-	    //	    | (+char_("a-zA-Z0-9_")[some_node_type]) >> * field_value;
+	    // // generic handler for anything else
+	    // //	    | (+char_("a-zA-Z0-9_")[some_node_type]) >> * field_value;
   
-	    | string("function_type")
-	    | string("parm_decl")
-	    | string("function_decl") >> + field_value
+	     | string("function_type")
+	     | string("void_type")
+	     | string("real_type")
+	     | string("tree_list")
+	     | string("type_decl")
+	     | string("parm_decl")
+	     | string("integer_type")
+	     | string("integer_cst")
+	     | string("function_decl") >> + field_value
 	    
 	    ;
 	  
@@ -117,18 +135,38 @@ namespace client
 	  lngt_field =  string("lngt:")>> +(digit[handle_lngt]);
 	  name_field   =  string("name:") >>   node_id[handle_name] ;
 	  scope_field  =  string("scpe:") >>   node_id[handle_scope] ;
-	  source_field =  string("srcp:") >>   filespec[handlefile];	  
+	  source_field =  string("srcp:")[handle_any] >>   filespec[handlefile];	  
 	  namespace_decls_field =  string("dcls:") >>   node_id[handle_decls];
 	  scope_field = string("scpe:") >> node_id[handle_scope];
 	  srcp_field = string("srcp:") >> filespec[handlefile];
-	  //	  strg_value =  +(char_("-:_a-zA-Z0-9"));	
-	  strg_value =  +(
-			  char_('-')
-			  | char_('_')
-			  | char_(':')
-			  |  char_("a-zA-Z0-9")
-			  );	
-	  strg_field = string("strg:")  >> strg_value[handle_strg];	
+	  strg_value = (+(
+	     		  char_('-')
+	     		  | char_('_')
+	     		  | char_(':')
+	     		  |  char_("a-zA-Z0-9")
+			  ))
+	    | (string("global type") |string("complex long double"))
+	    ;
+	
+	  strg_field = 
+	    string("strg:")  >> strg_value[handle_strg]
+	    ;	
+
+	   bits= (
+	   	 string("128")
+	   	 | string("64")
+	   	 | string("32")
+	   	 | string("16")
+	   	 | string("8")
+	   	 );
+
+	  note_field = 
+	    string("note:") >> (
+				string("artificial")  |
+				string("operator")  
+				)
+	    ;
+
 
 	  any_field   =  
 	    mngl_field 
@@ -138,71 +176,76 @@ namespace client
 	    | scope_field
 	    | source_field
 	    | srcp_field 
-	    //	    | strg_field
-
-	    | string("algn:") >> +(digit[handle_digits])
-	    | string("low:")  >> +(digit[handle_digits])
+	    | strg_field	    
+	    | string("algn:") >> bits
+	    | string("low :")  >> +(digit[handle_digits])
 	    | string("high:") >> -lit('-') >> +(digit[handle_digits])
-	    | string("accs:")				
+	    | string("accs:") >> string("pub")	
 	    | string("argt:")>>   node_id[handle_name]			
-	    | string("body:")>> string("undefined")				      				
-	    | string("bpos:")				
-	    | string("chain:") >>   node_id[handle_name]		
+	    | string("body:")>> string("undefined")   
+	    | string("bpos:")  >>   node_id[handle_name]
+	    | string("chain:") >>   node_id[handle_name]
 	    | string("domn:")>>   node_id[handle_name]			
 	    | string("flds:")	>>   node_id[handle_name]		
-	    | string("lang:")  >> string("C")				      
-	    | string("link:")  >> string("extern")
-	    | string("max:")>>   node_id[handle_name]			
+	    | string("lang:")  >> string("C")	       
+   	    | string("link:")  >> string("extern")
+	    | string("max :")>>   node_id[handle_name]			
 	    | string("min:")>>   node_id[handle_name]			
-	    | string("note:") >> (string("artificial") )	  		
-	    | string("prec:")						
+	    | note_field
+	    | string("prec:") >>  bits   
 	    | string("prms:") >>   node_id[handle_name]			
-	    | string("ptd:")  >>   node_id[handle_name]			
+	    | string("ptd :")  >>   node_id[handle_name]			
 	    | string("retn:") >>   node_id[handle_name]			
-	    | string("sign:") >> string("unsigned")						
+	    | string("sign:") >> string("unsigned")	      
 	    | string("size:")  >>   node_id[handle_name]
-	    | string("tag:")   
+	    | string("tag:")   >> string("struct")
 	    | string("type:") >>   node_id[handle_name]
-	    | string("used:")						
-	    | string("valu:") 
+	    | string("used:") >> +(digit[handle_digits])     
+	    | string("valu:") >>   node_id[handle_name]
 	    ;
 
-	  field_value= any_field[some_node_field] ;
+	  field_value= 
+	    any_field[some_node_field] 
+	    ;
 	  
 	  node_id = 
 	    lit('@')
 	    >> +digit
-	  //   //	    >> *space
-	  //   //	    >> *char_
 	    ;
 
 	  main_rule = 	  
 	    node_id >> 
 	    node_type 
-	    //+print[foo]
 	    ;
 
-	  secondary_line = 	  
+	  secondary_line = 
 	    namespace_decls_field 
 	    | + field_value
-	    // |	    +print[foo]
 	    ;
 
-	  either_lines = main_rule | secondary_line;
+	  either_lines = 
+	    main_rule | secondary_line
+	    ;
 	  
         }
 
       // have no spaces in the values
-      qi::rule<InputIterator,  spirit::utree()> 
-                 filespec,   
-		 strg_value,
-		 
-		 node_id
-		 ;
-      
+      qi::rule<InputIterator,  spirit::utree()> 	  
 
+      filespec,   
+		 strg_value,
+		 node_id,
+		 bits,
+
+      ;
+      
+      
+      // more complex space separated rules
       qi::rule<InputIterator, space_type , spirit::utree()> 
+		 ////////////////////////////
+
       main_rule,
+
 		 any_field,
 		 node_type,	 
 		 field_value,
@@ -214,10 +257,12 @@ namespace client
 		 source_field,
 		 srcp_field,
 		 strg_field, 
+		 note_field,
 		 secondary_line ,
-		 either_lines 
-		 ;
-
+		 either_lines ,
+		 identifier_node
+	;
+      
     };
 
 }
@@ -253,8 +298,7 @@ main()
 	std::cout << str << " Parses OK: " << std::endl;
       } else  {
       std::cout << "\n-------------------------\n";
-      std::cout << " Parse Failed: " << std::endl;
-      std::cout << str << std::endl;
+      std::cout << " Parse Failed: " << str << std::endl;
       std::cout << "-------------------------\n";
     }
   }
